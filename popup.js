3db3776d571e32524da2a4631a9575da85e83246
khuong nguyen <khuongnguyensac@gmail.com>
2021-02-03 22:47:40 -0800

    working

--- popup.js
+++ popup.js
@@ -1,6 +1,1 @@
-document.getELementById('submit').addEventListener('click', () => {
-  const images = document.querySelectorAll('img');
-  images.forEach((el) => {
-    el.remove();
-  });
-});
+
---
+++ search.js
@@ -0,0 +1,11 @@
+// alert('Hey yo');
+let count = 0;
+document.getElementById('submit').addEventListener('click', () => {
+  // const images = document.querySelectorAll('img');
+  // images.forEach((el) => {
+    //   el.remove();
+    // });
+  alert('Hey yo');
+  alert(count++);
+});
+
